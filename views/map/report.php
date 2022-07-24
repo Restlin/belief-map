24039c26fb00c265bd596708905517047edc8423
restlin <restlin@deviling>
2022-07-24 15:11:36 +0400

    [ADD] 1) fix css dashboard

--- views/map/report.php
+++ views/map/report.php
@@ -21,7 +21,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
 $datesRaw = array_keys(reset($cellCounts));
 $dates = [];
-foreach($datesRaw as $date) {
+foreach ($datesRaw as $date) {
     $dates[$date] = $date;
 }
 
@@ -35,36 +35,44 @@ $js = "$(function(){
 $this->registerJs($js);
 ?>
 <div class="map-report">
-    
+
     <div class="map-form">
-        <div class="row">
-            <div class="map-preview col-lg-4" style="padding-right: 30px;">
-                <h3 class="map-preview__title">customer position<br>map</h3>
-                <div class="map-preview__inner">
-                <?php
+        <div class="map-form__grid is-revert">
+            <div class="map-preview map-form__aside">
+                <div class="map-form__instruction">
+                    <h2>Instructions</h2>
+
+                    <p>Patients with the disease had a decreased adjusted HRQoL score of 4.7, reflecting a poorer. European guidelines recommend screening for patients with the disease5</p>
+                    <p>The European guidelines state ‘’ diagnostics tests should be used for initial assessment of a patient with newly diagnosed disease in order to evaluate their suitability for particular therapies’.</p>
+                    <p>The limitations of current therapies</p>
+                    <p>Current therapies are associated with substantial toxicity, which contributes to morbidity and mortality</p>
+                    <p>Current therapies are associated with substantial toxicity, due to non-specific effects, causing adverse events6
+                        <br>Over 50% of mortalities in the disease may be due to treatment related adverse events7 </p>
+                </div>
+                <div class="map-preview__inner is-new">
+                    <?php
                     $countRows = count($model->answers1);
                     $countColumns = count($model->answers2);
                     $rows = array_slice(['A', 'B', 'C', 'D', 'E'], 0, $countRows);
-                    $columns = array_slice(['5', '4', '3', '2', '1'], 5 - $countColumns, $countColumns);                    
-                    foreach($rows as $row) {
+                    $columns = array_slice(['5', '4', '3', '2', '1'], 5 - $countColumns, $countColumns);
+                    foreach ($rows as $row) {
                         echo Html::beginTag('div', ['class' => 'row']);
-                        foreach($columns as $column) {
+                        foreach ($columns as $column) {
                             $arrows = [];
-                            $cellCode = $row.$column;
+                            $cellCode = $row . $column;
                             $cellId = isset($cellIds[$cellCode]) ? $cellIds[$cellCode] : null;
-                            if(key_exists($cellId, $cellCounts)) {
-                                foreach($cellCounts[$cellId] as $date => $count) {
-                                    if($count > 99) {
+                            if (key_exists($cellId, $cellCounts)) {
+                                foreach ($cellCounts[$cellId] as $date => $count) {
+                                    if ($count > 99) {
                                         $count = "99+";
                                     }
                                     $arrows[] = Html::tag('div', $count, ['class' => "logbook-count hidden date-$date"]);
-
                                 }
                                 //$arrow = Html::tag('div', $count, ['class' => "logbook-count"]);
                             }
                             $arrow = implode("\n", $arrows);
                             $color = $colors[$cellCode];
-                            echo Html::tag("div", $cellCode.$arrow, ['class' => "cell", 'style' => "background: $color", 'data-code' => $cellCode]), "\n";
+                            echo Html::tag("div", $cellCode . $arrow, ['class' => "cell", 'style' => "background: $color", 'data-code' => $cellCode]), "\n";
                         }
                         echo Html::endTag('div');
                     }
@@ -81,59 +89,54 @@ $this->registerJs($js);
                             <div class="bullet"></div>
                         </div>
                     </parent>
-                </div>
-                <div class="map-form__instruction">
-                    <h2>Instructions</h2>
-                    <p>
-                        Patients with the disease had a decreased adjusted HRQoL score of 4.7, reflecting a poorer. European guidelines recommend screening for patients with the disease<br><br>
-
-                        The European guidelines state ‘’ diagnostics tests should be used for initial assessment of a patient with newly diagnosed disease in order to evaluate their suitability for particular therapies’.
-                        The limitations of current therapies.
-                    </p>
-                </div>
+                </div>                
             </div>
-            <div class="col-lg-8 report-data-container">
+            <div class="map-form__main">
+                <div class="report-data-container">
 
-                <div class="date-slider" style="margin-bottom: 20px;">
-                    <div class="date-slider__start">Start  <span><?=reset($dates) ?></span></div>
-                    <input type="range" class="date-slider__input js-date-slider" data-dates="<?=implode(', ', $dates) ?>" id="select-date">
-                    <div class="date-slider__end">Today<span><?=end($dates) ?></span></div>
-                </div>
-                
-                <div class="scroll-area">
-                    <?php Pjax::begin(); ?>
+                    <div class="date-slider" style="margin-bottom: 20px;">
+                        <div class="date-slider__start">Start  <span><?= reset($dates) ?></span></div>
+                        <input type="range" class="date-slider__input js-date-slider" data-dates="<?= implode(', ', $dates) ?>" id="select-date">
+                        <div class="date-slider__end">Today<span><?= end($dates) ?></span></div>
+                    </div>
 
-                    <?= GridView::widget([
-                        'dataProvider' => $dataProvider,
-                        'filterModel' => $searchModel,
-                        'summary' => null,
-                        'columns' => [
-                            [
-                                'attribute' => 'contactName',
-                                'label' => 'Contact',
-                                'value' => function(Logbook $model) {
-                                    return $model->contact->name;
-                                },
-                                'filter' => $contacts,
-                            ],
-                            [
-                                'attribute' => 'cell_id',
-                                'label' => 'Position',
-                                'value' => function(Logbook $model) {
-                                    $codes = Cell::getCodeList($model->cell->answer1->map_id);
-                                    return $codes[$model->cell->id];
-                                },
-                                'filter' => false,
-                            ],
-                            [
-                                'attribute' => 'date_in',
-                                'format' => 'datetime',
-                                'filter' => false
-                            ],
-                        ],
-                    ]); ?>
+                    <div class="scroll-area">
+                        <?php Pjax::begin(); ?>
 
-                    <?php Pjax::end(); ?>
+                        <?=
+                        GridView::widget([
+                            'dataProvider' => $dataProvider,
+                            'filterModel' => $searchModel,
+                            'summary' => null,
+                            'columns' => [
+                                [
+                                    'attribute' => 'contactName',
+                                    'label' => 'Contact',
+                                    'value' => function (Logbook $model) {
+                                        return $model->contact->name;
+                                    },
+                                    'filter' => $contacts,
+                                ],
+                                [
+                                    'attribute' => 'cell_id',
+                                    'label' => 'Position',
+                                    'value' => function (Logbook $model) {
+                                        $codes = Cell::getCodeList($model->cell->answer1->map_id);
+                                        return $codes[$model->cell->id];
+                                    },
+                                    'filter' => false,
+                                ],
+                                [
+                                    'attribute' => 'date_in',
+                                    'format' => 'datetime',
+                                    'filter' => false
+                                ],
+                            ],
+                        ]);
+                        ?>
+
+<?php Pjax::end(); ?>
+                    </div>
                 </div>
             </div>
         </div>
